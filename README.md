167e7062cc41ff8db40b97e16ea02dce501a7b47
agus <aalma@mail.austral.edu.ar>
2024-03-21 17:26:55 -0300

    created an empty proyect

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Trackinesis
 App para poder cargar tu rutina y ejercicios ya sea de gimnasio o deporte en general
+Para poder correr el proyectoReact hay que tener Node.jsx npm y hacer npm start
---
+++ testReact.jsx
@@ -0,0 +1,14 @@
+function MyButton() {
+    return (
+        <button>Im a button</button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1> Welcome to the Test</h1>
+            <MyButton />
+        </div>
+    );
+}
