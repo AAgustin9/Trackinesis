ba5d983b25582b9db0f798f02d64592ee82e94ca
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-03-24 12:27:53 -0300

    signup2val2.0

--- frontend/src/SignupStepTwoValidation.js
+++ frontend/src/SignupStepTwoValidation.js
@@ -1,7 +1,40 @@
-import React from "react";
+function Validation(values) {
+    let error = {}
 
-function Validation() {
+    if (!values.age.trim()) {
+        error.age = "Age should not be empty";
+    } 
+    else if (isNaN(values.age)) {
+        error.age = "Age must be a number";
+    } 
+    else if (parseInt(values.age) > 117) {
+        error.age = "Age must be smaller than 117";
+    }
 
+    if (!values.weight.trim()) {
+        error.weight = "Weight should not be empty";
+    } 
+    else if (isNaN(values.weight)) {
+        error.weight = "Weight must be a number";
+    } 
+    else if (parseFloat(values.weight) <= 0) {
+        error.weight = "Weight must be greater than 0";
+    }
+
+    if (!values.height.trim()) {
+        error.height = "Height should not be empty";
+    } 
+    else if (isNaN(values.height)) {
+        error.height = "Height must be a number";
+    } 
+    else if (parseFloat(values.height) <= 0) {
+        error.height = "Height must be greater than 0";
+    }
+
+    if (!values.gender) {
+        error.gender = "Gender should be selected";
+    }
+    return error;
 }
 
 export default Validation
