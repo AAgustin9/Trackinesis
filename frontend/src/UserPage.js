92e61005cd0a57dcd0b18b1de6172824a629222b
agus <aalma@mail.austral.edu.ar>
2024-03-24 17:31:31 -0300

    bettered css

--- frontend/src/UserPage.js
+++ frontend/src/UserPage.js
@@ -6,7 +6,7 @@ function UserPage() {
     
 
     return (
-        <div className='d-flex justify-content-center allign-items-center bg-primary vh-100'>
+        <div className='main-page-format'>
             <div className='bg-white p-4 rounded w-25'>
                 <h2>User Page</h2>
                 
