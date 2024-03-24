948e474b49144a9619fff36105a4a689331b2f00
agus <aalma@mail.austral.edu.ar>
2024-03-24 16:42:58 -0300

    added delete button (no function) to user page

--- frontend/src/UserPage.js
+++ frontend/src/UserPage.js
@@ -1,14 +1,25 @@
 import React from "react";
+import './styles.css';
 
 function UserPage() {
+
+    
 
     return (
         <div className='d-flex justify-content-center allign-items-center bg-primary vh-100'>
             <div className='bg-white p-4 rounded w-25'>
                 <h2>User Page</h2>
+                
+                
+                
+                
             </div>   
+            <div className="bottom-right-button-container"> 
+                    <button id="deleteAccountButton" className='btn btn-success w-100 rounded-0'> Delete My Account</button>
+                </div>
+            
         </div>
-    
+
     
     )
 }
