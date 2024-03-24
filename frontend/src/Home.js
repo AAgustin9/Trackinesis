64dc8933e2f8f84b98d2f34cdfc6e2a3303545e9
agus <aalma@mail.austral.edu.ar>
2024-03-24 09:28:15 -0300

    added axios to json

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className='d-flex justify-content-center allign-items-center bg-primary vh-100'>
+            <div className='bg-white p-3 rounded w-25'>
+                <h2>Hola getName()</h2>
+                <Link> to="/UserPage" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>User Page </Link>
+            </div>
+        </div>
     )
 }
 
---
+++ frontend/src/UserPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function UserPage() {
+
+    return (
+        <div className='d-flex justify-content-center allign-items-center bg-primary vh-100'>
+            <div className='bg-white p-4 rounded w-25'>
+                <h2>User Page</h2>
+            </div>   
+        </div>
+    
+    
+    )
+}
